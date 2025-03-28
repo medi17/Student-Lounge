abf3b74fdd4cab1a14fc24a58e5d9b2c275b5e3a
Medan <medanabebe17@gmail.com>
2025-03-28 22:05:19 +0300

    modified header.tsx

--- Frontend/src/components/shared/header.tsx
+++ Frontend/src/components/shared/header.tsx
@@ -1,5 +1,6 @@
-import {JSX, useState} from "react"
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { JSX, useState } from "react"
+import { NavLink } from 'react-router-dom'
+import { faMagnifyingGlass, faCartShopping } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const header = ():JSX.Element => {
@@ -14,25 +15,73 @@ const header = ():JSX.Element => {
           <div className="flex justify-between items-center m-5 py-3 px-7 bg-white shadow-sm rounded-[50px] lg:mx-16">
                <h1 className="text-[24px] font-semibold text-Crimson">LOUNGE</h1>
                <ul className={toggleState ?"nav active-nav flex justify-between items-center gap-8 text-[20px]" : "nav flex justify-between items-center gap-8 text-[20px]"} >
-                    <li className="hover:text-Crimson hover:text-[22px] hover:font-medium"><a href="#">Home</a></li>
-                    <li className="hover:text-Crimson hover:text-[22px] hover:font-medium"><a href="#">About</a></li>
-                    <li className="hover:text-Crimson hover:text-[22px] hover:font-medium"><a href="#">Contact</a></li>
+                    <li className="hover:text-Crimson hover:text-[22px] hover:font-medium"><NavLink to="/">Home</NavLink></li>
+                    <li className="hover:text-Crimson hover:text-[22px] hover:font-medium"><NavLink to="/menu">Menu</NavLink></li>
+                    <li className="hover:text-Crimson hover:text-[22px] hover:font-medium"><NavLink to="/contact">Contact</NavLink></li>
                </ul>
-               <div className="hidden md:block">
-                    <div className="flex justify-center items-center gap-2">
-                         <FontAwesomeIcon icon={faMagnifyingGlass} className="text-[20px] cursor-pointer"/>
-                         <button className="cursor-pointer bg-Crimson text-white text-[20px] font-medium py-1 px-3 border-2 border-Crimson rounded-3xl hover:text-Crimson hover:bg-white">
-                              Login
-                         </button>
+               <div className="flex justify-center items-center gap-4">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} className="text-[20px] cursor-pointer" />
+                    <FontAwesomeIcon icon={faCartShopping} className="text-[20px] cursor-pointer"/>
+                    <div className="hidden md:block">
+                         <NavLink to="/Login">
+                              <button className="cursor-pointer bg-Crimson text-white text-[20px] font-medium py-1 px-3 border-2 border-Crimson rounded-3xl hover:text-Crimson hover:bg-white">
+                                   Login
+                              </button>
+                         </NavLink>
                     </div>
-               </div>
-               <div className= "cursor-pointer flex flex-col items-center p-2 border-3 border-Crimson rounded-[12px] md:hidden" onClick={toggleMenu}>
-                    <div className={toggleState ? 'w-6 h-1 rounded-xs bg-Crimson transform translate-y-[8px] rotate-45' : "w-6 h-1 rounded-xs bg-Crimson"} ></div>
-                    <div className={toggleState ? 'w-6 h-1 mt-1 rounded-xs opacity-0' : "w-6 h-1 mt-1 rounded-xs bg-Crimson"}></div>
-                    <div className={toggleState ? 'w-6 h-1 mt-1 rounded-xs bg-Crimson transform -translate-y-[8px] -rotate-45' : "w-6 h-1 mt-1 rounded-xs bg-Crimson"}></div>
+                    <div className= "cursor-pointer flex flex-col items-center p-2 border-3 border-Crimson rounded-[12px] md:hidden" onClick={toggleMenu}>
+                         <div className={toggleState ? 'w-6 h-1 rounded-xs bg-Crimson transform translate-y-[8px] rotate-45' : "w-6 h-1 rounded-xs bg-Crimson"} ></div>
+                         <div className={toggleState ? 'w-6 h-1 mt-1 rounded-xs opacity-0' : "w-6 h-1 mt-1 rounded-xs bg-Crimson"}></div>
+                         <div className={toggleState ? 'w-6 h-1 mt-1 rounded-xs bg-Crimson transform -translate-y-[8px] -rotate-45' : "w-6 h-1 mt-1 rounded-xs bg-Crimson"}></div>
+                    </div>
                </div>
           </div>
      )
 }
 
 export default header
+
+// import { JSX, useState } from "react"
+// import { NavLink } from 'react-router-dom'
+// import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+// const header = ():JSX.Element => {
+     
+//      const [toggleState, setToggleState] = useState<boolean>(false)
+
+//      const toggleMenu = () => {
+//           setToggleState(!toggleState);
+//      }
+
+//      return (
+//           <div className="flex justify-between items-center m-5 py-3 px-7 bg-white shadow-sm rounded-[50px] lg:mx-16">
+//                <h1 className="text-[24px] font-semibold text-Crimson">LOUNGE</h1>
+//                <div className="nav active-nav flex justify-between items-center w-[60%]">
+//                     <ul className={toggleState ?"nav active-nav flex justify-between items-center gap-8 text-[20px]" : "nav flex justify-between items-center gap-8 text-[20px]"} >
+//                          <li className="hover:text-Crimson hover:text-[22px] hover:font-medium"><NavLink href="/">Home</NavLink></li>
+//                          <li className="hover:text-Crimson hover:text-[22px] hover:font-medium"><a href="#about">About</a></li>
+//                          <li className="hover:text-Crimson hover:text-[22px] hover:font-medium"><a href="#contact">Contact</a></li>
+//                     </ul>
+//                     <div className="">
+//                          <div className="flex justify-center items-center gap-2">
+//                               <FontAwesomeIcon icon={faMagnifyingGlass} className="text-[20px] cursor-pointer" />
+//                               <NavLink to="/Login">
+//                                    <button className="cursor-pointer bg-Crimson text-white text-[20px] font-medium py-1 px-3 border-2 border-Crimson rounded-3xl hover:text-Crimson hover:bg-white">
+//                                         Login
+//                                    </button>
+//                               </NavLink>
+//                          </div>
+//                     </div>
+//                </div>
+//                <div className= "cursor-pointer flex flex-col items-center p-2 border-3 border-Crimson rounded-[12px] md:hidden" onClick={toggleMenu}>
+//                     <div className={toggleState ? 'w-6 h-1 rounded-xs bg-Crimson transform translate-y-[8px] rotate-45' : "w-6 h-1 rounded-xs bg-Crimson"} ></div>
+//                     <div className={toggleState ? 'w-6 h-1 mt-1 rounded-xs opacity-0' : "w-6 h-1 mt-1 rounded-xs bg-Crimson"}></div>
+//                     <div className={toggleState ? 'w-6 h-1 mt-1 rounded-xs bg-Crimson transform -translate-y-[8px] -rotate-45' : "w-6 h-1 mt-1 rounded-xs bg-Crimson"}></div>
+//                </div>
+//           </div>
+//      )
+// }
+
+// export default header
+
