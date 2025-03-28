1bbfabf05cd5c391e28d94faae09026af14ec8d1
Medan <medanabebe17@gmail.com>
2025-03-28 22:12:11 +0300

    Added routing

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,26 @@
 import Home from "./pages/home"
+import Menu from "./pages/menu"
+import Contact from "./pages/contact"
+import Login from "./pages/login"
+import { createBrowserRouter } from 'react-router-dom'
 
-const App = () => {
-     return (
-          <div>
-               <Home />
-          </div>
-     )
-}
+const App = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/menu',
+    element: <Menu />
+  },
+  {
+    path: '/contact',
+    element: <Contact />
+},
+  {
+    path: '/login',
+    element: <Login />
+  },  
+])
 
 export default App
