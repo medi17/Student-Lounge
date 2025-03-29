aa3d504fd4ddb87774e77a3ad3780f66b918cef0
Medan <medanabebe17@gmail.com>
2025-03-29 21:24:56 +0300

    increeased the size of backgound image

--- Frontend/src/components/home/hero.tsx
+++ Frontend/src/components/home/hero.tsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const hero = ():JSX.Element => {
      return (
-          <div className="hero-section  mx-6 rounded-[30px] md:mx-16 md:h-[400px]">
+          <div className="hero-section mx-6 rounded-[30px] md:mx-16 md:h-[420px]">
                <div className="hero pt-10 pb-5 px-5 text-white md:pt-28 md:px-10" >
                     <h1 className="text-4xl md:text-[72px]"><span className="font-semibold">Hungry?</span> Get it fast.</h1>
                     <p className="text-sm py-2 md:text-2xl">Fast, fresh and just the way you like it!</p>
