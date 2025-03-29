e3c45e4111346b4b30e50321e429a9063989f49d
Medan <medanabebe17@gmail.com>
2025-03-29 21:42:02 +0300

    deleted background color

--- Frontend/src/components/home/about.tsx
+++ Frontend/src/components/home/about.tsx
@@ -1,9 +1,9 @@
 
 const about = () => {
      return (
-          <div className="about bg-black text-white text-center mx-6 rounded-[30px] md:mx-16 md:h-[400px]">
-               <h1 className="pt-5 text-3xl md:pt-16 md:mx-5 md:text-4xl md:font-medium lg:pt-28">About us</h1>
-               <p className="p-6 md:px-32 md:text-xl">More than just a lounge, we're a space where good food
+          <div className="about text-white text-center mx-6 rounded-[30px] md:mx-16 md:h-[400px]">
+               <h1 className="pt-5 text-2xl md:pt-16 md:mx-5 md:text-4xl md:font-medium lg:pt-28">About us</h1>
+               <p className="p-6 md:px-32 text-xs md:text-xl">More than just a lounge, we're a space where good food
                     and good company come together.  Imagine taking a study
                     break with a warm cup of coffee or sharing a quick meal
                     with friends.  We're here to nourish your mind and body,
