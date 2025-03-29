898cc11fd40859224daa295777c9e4ca5da37e6b
Medan <medanabebe17@gmail.com>
2025-03-29 19:38:23 +0300

    wrapped the app component with context provider

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+
+import MenuContextProvider from './context/MenuContext'
+
 import './index.css'
 import App from "./App"
 
+const AppProviders: React.FC<React.PropsWithChildren<{}>> = ({children}) => {
+  return (
+    <MenuContextProvider>
+      {children}
+    </MenuContextProvider>
+  )
+}
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <AppProviders>
+    <RouterProvider router = {App} />
+  </AppProviders>, 
 )
