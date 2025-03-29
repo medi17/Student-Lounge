c049300f9db381dcdcffb5f7aa0bbfc7cc9bde7b
Medan <medanabebe17@gmail.com>
2025-03-29 21:21:44 +0300

    added some padding

--- Frontend/src/components/home/catagories/drinks.tsx
+++ Frontend/src/components/home/catagories/drinks.tsx
@@ -15,7 +15,7 @@ const drinks = () => {
      }
 
      return (
-          <div className="scroll bg-white flex justify-start gap-5 overflow-x-scroll mb-5 mx-2 rounded-[30px] md:gap-10">
+          <div className="scroll bg-white flex justify-start gap-5 overflow-x-scroll mb-5 mx-2 rounded-[30px] md:gap-10 px-10 py-5">
                {
                     drinkItems.map((item:FoodItem, index:number) => {
                          return (
