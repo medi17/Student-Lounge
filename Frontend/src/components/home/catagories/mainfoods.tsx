40ae92a6794bdd2bb1caba6355d120d858d9e4c9
Medan <medanabebe17@gmail.com>
2025-03-29 19:42:25 +0300

    added context and styled the error message

--- Frontend/src/components/home/catagories/mainfoods.tsx
+++ Frontend/src/components/home/catagories/mainfoods.tsx
@@ -1,16 +1,18 @@
 import FoodComponent from "./food-component"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFaceSadTear } from "@fortawesome/free-solid-svg-icons"
 import { useContext } from "react"
 import { MenuContext } from "../../../context/MenuContext"
 import { FoodItem } from "../../../types/types"
 
 const Mainfoods = () => {
 
-     const foodItems = useContext(MenuContext)
+     const FoodListObject = useContext(MenuContext)
+     const foodItems = FoodListObject?.foodItems || []
 
      if (!foodItems) {
-          return <div>No food items available.</div>;
+          return <div className="ml-5 text-2xl font-bold">Sorry no food items are available <FontAwesomeIcon icon={faFaceSadTear} className="text-yellow-600 text-3xl"/></div>;
      }
-
      return (
           <div className="scroll bg-white flex justify-start gap-5 overflow-x-scroll mb-5 mx-2 rounded-[30px] md:gap-10">
                {
