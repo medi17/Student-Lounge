09c07a454bc12dd4dd6e5c1f9a6998aed6bfa17a
Medan <medanabebe17@gmail.com>
2025-04-03 22:36:56 +0300

    changed footer component directory

--- Frontend/src/pages/home.tsx
+++ Frontend/src/pages/home.tsx
@@ -3,7 +3,7 @@ import Header from "../components/shared/header"
 import Hero from "../components/home/hero"
 import Catagories from "../components/home/catagories/catagories"
 import About from "../components/home/about"
-import Footer from "../components/home/footer"
+import Footer from "../components/shared/footer"
 
 const home = ():JSX.Element => {
      return (
