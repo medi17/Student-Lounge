d012ef3d91e5e60940a0353342e1a394ff12eb1d
Medan <medanabebe17@gmail.com>
2025-04-03 22:34:51 +0300

    made whole cart page

---
+++ Frontend/src/pages/cart.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/shared/header"
+import CartCard from "../components/cart/cartCard"
+import Footer from "../components/shared/footer"
+
+const cart = () => {
+     return (
+          <div>
+               <Header />
+               <CartCard />
+               <Footer />
+          </div>
+     )
+}
+
+export default cart
