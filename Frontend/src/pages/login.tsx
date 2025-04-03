63ed3ed884129d8fffab85bab9d2c019d49186ec
Medan <medanabebe17@gmail.com>
2025-04-03 22:35:57 +0300

    made the whole log in component

---
+++ Frontend/src/pages/login.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/shared/header"
+import Footer from "../components/shared/footer"
+import LoginCard from "../components/Log/loginCard"
+const about = () => {
+  return (
+    <div>
+      <Header />
+      <LoginCard />
+      <Footer />
+    </div>
+  )
+}
+
+export default about
