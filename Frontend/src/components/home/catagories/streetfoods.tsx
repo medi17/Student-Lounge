957979406c4363a097623d9480d7a974b0165742
Medan <medanabebe17@gmail.com>
2025-04-05 22:54:59 +0300

    updated after the data

--- Frontend/src/components/home/catagories/streetfoods.tsx
+++ Frontend/src/components/home/catagories/streetfoods.tsx
@@ -8,7 +8,7 @@ import { FoodItem } from "../../../types/types"
 const streetfoods = () => {
 
      const FoodListObject = useContext(MenuContext)
-     const streetFoodItems = FoodListObject?.streetFoodItems || []
+     const streetFoodItems = FoodListObject?.foodItems || []
 
      if (!streetFoodItems) {
           return <div className="ml-5 text-2xl font-bold">Sorry no food items are available <FontAwesomeIcon icon={faFaceSadTear} className="text-yellow-600 text-3xl"/></div>;
@@ -17,19 +17,21 @@ const streetfoods = () => {
      return (
           <div className="scroll bg-white flex justify-start gap-5 overflow-x-scroll mb-5 mx-2 rounded-[30px] md:gap-10 px-3 py-5 md:px-10">
                {
-                    streetFoodItems.map((item:FoodItem, index:number) => {
-                         return (
-                              <FoodComponent
-                                   key={index}
-                                   id={item.id}
-                                   name={item.name}
-                                   duration = {item.duration}
-                                   description={item.description}
-                                   price={item.price}
+                    streetFoodItems.map((item: FoodItem, index: number) => (                         
+                         item.catagory === "street" && (     
+                              < FoodComponent
+                                   key = { index }
+                                   id = { item.id }
+                                   name = { item.name }
+                                   duration = { item.duration }
+                                   description = { item.description }
+                                   price = { item.price }
                                    image={item.image}
+                                   catagory={item.catagory}
                               />
-                         )
-                    })
+                         ) 
+                    
+                    ))
                }
           </div>
      )
