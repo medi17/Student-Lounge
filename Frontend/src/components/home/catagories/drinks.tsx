edf494fddddfd9bf6e7726176d529968b42d0082
Medan <medanabebe17@gmail.com>
2025-04-05 22:54:08 +0300

    updated after the data

--- Frontend/src/components/home/catagories/drinks.tsx
+++ Frontend/src/components/home/catagories/drinks.tsx
@@ -8,7 +8,7 @@ import { FoodItem } from "../../../types/types"
 const drinks = () => {
 
      const FoodListObject = useContext(MenuContext)
-     const drinkItems = FoodListObject?.drinkItems || []
+     const drinkItems = FoodListObject?.foodItems || []
 
      if (!drinkItems) {
           return <div className="ml-5 text-2xl font-bold">Sorry no food items are available <FontAwesomeIcon icon={faFaceSadTear} className="text-yellow-600 text-3xl"/></div>;
@@ -17,19 +17,21 @@ const drinks = () => {
      return (
           <div className="scroll bg-white flex justify-start gap-5 overflow-x-scroll mb-5 mx-2 rounded-[30px] md:gap-10 px-3 py-5 md:px-10">
                {
-                    drinkItems.map((item:FoodItem, index:number) => {
-                         return (
-                              <FoodComponent
-                                   key={index}
-                                   id={item.id}
-                                   name={item.name}
-                                   duration = {item.duration}
-                                   description={item.description}
-                                   price={item.price}
-                                   image={item.image}
+                    drinkItems.map((item: FoodItem, index: number) => (                         
+                         item.catagory === "drink" && (     
+                              < FoodComponent food = {item}
+                                   // key = { index }
+                                   // id = { item.id }
+                                   // name = { item.name }
+                                   // duration = { item.duration }
+                                   // description = { item.description }
+                                   // price = { item.price }
+                                   // image={item.image}
+                                   // catagory={item.catagory}
                               />
                          )
-                    })
+                    
+                    ))
                }
           </div>
      )               
