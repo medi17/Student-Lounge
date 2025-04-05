96fdced7c6f8cfec0a9472ad69a0a6c75916d22f
Medan <medanabebe17@gmail.com>
2025-04-05 22:54:48 +0300

    updated after the data

--- Frontend/src/components/home/catagories/mainfoods.tsx
+++ Frontend/src/components/home/catagories/mainfoods.tsx
@@ -8,27 +8,29 @@ import { FoodItem } from "../../../types/types"
 const Mainfoods = () => {
 
      const FoodListObject = useContext(MenuContext)
-     const foodItems = FoodListObject?.foodItems || []
+     const mainItems = FoodListObject?.foodItems || []
 
-     if (!foodItems) {
+     if (!mainItems) {
           return <div className="ml-5 text-2xl font-bold">Sorry no food items are available <FontAwesomeIcon icon={faFaceSadTear} className="text-yellow-600 text-3xl"/></div>;
      }
      return (
           <div className="scroll bg-white flex justify-start gap-5 overflow-x-scroll mb-5 mx-2 rounded-[30px] md:gap-10 px-3 py-5 md:px-10">
                {
-                    foodItems.map((item:FoodItem, index:number) => {
-                         return (
-                              <FoodComponent
-                                   key={index}
-                                   id={item.id}
-                                   name={item.name}
-                                   duration = {item.duration}
-                                   description={item.description}
-                                   price={item.price}
+                    mainItems.map((item: FoodItem, index: number) => (                         
+                         item.catagory === "main" && (     
+                              < FoodComponent
+                                   key = { index }
+                                   id = { item.id }
+                                   name = { item.name }
+                                   duration = { item.duration }
+                                   description = { item.description }
+                                   price = { item.price }
                                    image={item.image}
-                              /> 
-                         )
-                    })
+                                   catagory={item.catagory}
+                              />
+                         ) 
+                    
+                    ))
                }
           </div>
      )
