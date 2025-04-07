9c66d5cbb0bcc0c320f2acf67faeaf9795422265
Medan <medanabebe17@gmail.com>
2025-04-07 23:20:13 +0300

    added increase action

--- Frontend/src/hooks/UseReducer.ts
+++ Frontend/src/hooks/UseReducer.ts
@@ -1,16 +1,36 @@
-import { ActionType, StateType } from "../types/types"
+import { ReducerAction, CartStateType, CartItemType } from "../types/types"
 
-export const IntialState:StateType = {
-     food: []
+export const InitialState: CartStateType = {
+  cart: []
 }
 
 
-const cartReducer = (state: StateType, action:ActionType) => {
+const cartReducer = (state: CartStateType, action:ReducerAction):CartStateType => {
      switch(action.type){
           case "Add":
-               return { ...state, food: action.payload }
+               return {
+                    ...state, 
+                    cart: [...state.cart, action.payload] as CartItemType[]
+               }
           case "Remove":
+               // return {
+               //      ...state,
+
+               //      cart: state.cart.filter((food: CartItemType) => (
+               //           if(food.id !== action.payload.id)
+               //      ) 
+               //      )
+               // }
           case "Increase":
+               return {
+                    ...state,
+                    cart: state.cart.map((food: CartItemType) => {
+                         if (action.payload && food.id === action.payload.id) {
+                              return { ...food, quantity: food.quantity + 1 }
+                         }
+                         return food
+                    })
+               }
           case "Decrease":
           default:
                return state
