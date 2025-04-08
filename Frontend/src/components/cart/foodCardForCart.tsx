630bfe68c51ddc212f0e19bc11f2d89aece89017
Medan <medanabebe17@gmail.com>
2025-04-09 00:33:05 +0300

    Added context error handling

--- Frontend/src/components/cart/foodCardForCart.tsx
+++ Frontend/src/components/cart/foodCardForCart.tsx
@@ -8,17 +8,24 @@ const foodCardForCart = ({food}: FoodProps) => {
      const  { id, name, image, price, quantity } = food
      
      const cartContext = useContext(CartContext)
+
+     if (!cartContext) {
+          throw new Error("CartContext is not provided");
+     }
+
      const cart = cartContext?.cart.cart || []
      const dispatch = cartContext?.dispatch
 
-     const Increase = ()=> {
-          if (dispatch) {
+     const Increase = (id: number) => {
+          const index = cart.findIndex(foodItem => foodItem.id === id)
+          if (cart[index].quantity < 10) {
                dispatch({type: "Increase", payload: food})
           }
      }
+
      const Decrease = (id: number)=> {
           const index = cart.findIndex(foodItem => foodItem.id === id)
-          if (cart[index].quantity > 1 && dispatch) {
+          if (cart[index].quantity > 1) {
                dispatch({type: "Decrease", payload: food})
           }
      }
@@ -39,14 +46,14 @@ const foodCardForCart = ({food}: FoodProps) => {
                               >-</button>
                               <p className="text-2xl font-medium">{quantity}</p>
                               <button className="bg-Crimson text-white font-medium text-xl px-2 border-2 border-Crimson rounded-xl hover:text-Crimson hover:bg-white cursor-pointer"
-                                   onClick={() => Increase()}
+                                   onClick={() => Increase(id)}
                               >+</button>
                          </div>
                     </div>
                </div>
                
                <button className="px-2 py-2 rounded-xl cursor-pointer hover:text-Crimson"
-                    onClick={() => { dispatch && dispatch({type: "Remove", payload: food})}}
+                    onClick={() => {dispatch({type: "Remove", payload: food})}}
                ><FontAwesomeIcon icon={faTrashCan} className="text-xl"/></button>
                
           </div>               
