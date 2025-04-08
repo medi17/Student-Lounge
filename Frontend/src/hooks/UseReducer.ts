676904f84302d12b4f777816568ab1c2c6648a51
Medan <medanabebe17@gmail.com>
2025-04-09 00:32:21 +0300

    updated the UseReducer

--- Frontend/src/hooks/UseReducer.ts
+++ Frontend/src/hooks/UseReducer.ts
@@ -9,32 +9,36 @@ const cartReducer = (state: CartStateType, action:ReducerAction):CartStateType =
      switch(action.type){
           case "Add":
                return {
-                    ...state, 
-                    cart: [...state.cart, action.payload] as CartItemType[]
+                    cart:[...state.cart, action.payload] as CartItemType[]
                }
           case "Remove":
                return {
-                    ...state,
-                    cart: state.cart.filter((food: CartItemType) => food.id !== action.payload?.id)
+                    cart: state.cart.filter((foodItem: CartItemType) => 
+                         foodItem.id !== action.payload.id
+                    )
                }
           case "Increase":
                return {
                     ...state,
-                    cart: state.cart.map((food: CartItemType) => {
-                         if (action.payload && food.id === action.payload.id) {
-                              return { ...food, quantity: food.quantity + 1 }
+                    cart: state.cart.map((foodItem: CartItemType) => {
+                         if (foodItem.id === action.payload.id) {
+                              return {
+                                   ...foodItem, quantity: foodItem.quantity + 1
+                              }
                          }
-                         return food
+                         return foodItem
                     })
                }
           case "Decrease":
                return {
                     ...state,
-                    cart: state.cart.map((food: CartItemType) => {
-                         if (action.payload && food.id === action.payload.id) {
-                              return { ...food, quantity: food.quantity - 1 }
+                    cart: state.cart.map((foodItem: CartItemType) => {
+                         if (foodItem.id === action.payload.id) {
+                              return {
+                                   ...foodItem, quantity: foodItem.quantity - 1
+                              }
                          }
-                         return food
+                         return foodItem
                     })
                }
           default:
