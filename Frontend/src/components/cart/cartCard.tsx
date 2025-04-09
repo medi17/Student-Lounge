08df9970a926c31e6e7c4d47c657d0099462d6a7
Medan <medanabebe17@gmail.com>
2025-04-09 20:26:21 +0300

    Added order section and changed styles

--- Frontend/src/components/cart/cartCard.tsx
+++ Frontend/src/components/cart/cartCard.tsx
@@ -4,7 +4,7 @@ import { CartItemType } from "../../types/types"
 import { CartContext } from "../../context/CartContext"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHeartCrack } from "@fortawesome/free-solid-svg-icons"
-import OrderSummary from "./orderSummary"
+import Order from "./order"
 
 const cartCard = () => {
 
@@ -12,13 +12,13 @@ const cartCard = () => {
      const cartItems = cart?.cart.cart || []
 
      return (
-          <div className="bg-white shadow-2xl rounded-[30px] py-3 px-3 mx-5 min-w-[260px] md:px-7 md:min-w-[312px] lg:mx-16 md:h-[450px] overflow-hidden">
+          <div className="bg-white shadow-2xl rounded-[30px] py-5 px-6 mx-5 min-w-[260px] md:px-7 md:min-w-[312px] lg:mx-16 md:h-[450px] overflow-hidden">
                <h1 className="text-2xl font-medium">My Orders</h1>
                <div className="md:grid md:grid-cols-2 md:place-items-center">                    
                     <div className="scroll overflow-y-scroll max-h-[450px]">
                          {
                               cartItems.length === 0 ? (
-                                   <div className="my-18 text-4xl font-medium">No food items in your cart <FontAwesomeIcon icon={faHeartCrack} className="text-red-600 text-3xl" /></div>
+                                   <div className="my-18 text-3xl font-medium md:text-4xl">No food items in your cart <FontAwesomeIcon icon={faHeartCrack} className="text-red-600 text-3xl" /></div>
                               ) : (
                                    
                                    cartItems.map((food: CartItemType) => (
@@ -28,7 +28,7 @@ const cartCard = () => {
                               )
                          }
                     </div>
-                    <OrderSummary/>                    
+                    <Order cartItems={cartItems } />                    
                </div>
           </div>
      )
