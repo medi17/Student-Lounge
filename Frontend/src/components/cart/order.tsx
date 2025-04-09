059f1a4ab944b9f4f45cc02e0d6c4bc62f399c62
Medan <medanabebe17@gmail.com>
2025-04-09 20:27:15 +0300

    added order component

---
+++ Frontend/src/components/cart/order.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+import { CartItemType } from "../../types/types"
+import Delivery from "./delivery"
+import OrderSummary from "./orderSummary"
+
+type Props = {
+    cartItems: CartItemType[]
+}
+
+const Order = ({cartItems}:Props) => {
+
+     const [toOrdersummary, setToOrdersummary] = useState<boolean>(false)
+
+     return (
+          <div className={cartItems.length === 0 ? "hidden":  "block"}>
+               <div className={toOrdersummary? "hidden": "block"}>
+                    <Delivery setToOrdersummary = {setToOrdersummary}/>
+               </div>
+
+               <div className={toOrdersummary? "block text-center mt-8 md:mx-10": "hidden"}>
+                    <OrderSummary />
+               </div>
+          </div>
+     )
+}
+
+export default Order
