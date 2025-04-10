5d72791129f02afbbce796b7e08e514821767666
Medan <medanabebe17@gmail.com>
2025-04-10 12:45:03 +0300

    Added main contact message component

---
+++ Frontend/src/components/contact/Contact_message.tsx
@@ -0,0 +1,31 @@
+import ContactCatagories from "./contact_catagories"
+import Form from "./form"
+
+
+const ContactMessage = () => {
+     return (
+          <div className="m-5 py-3 px-7 rounded-3xl lg:mx-16 md:flex md:justify-between md:gap-10 lg:gap-20">
+               <div className="pt-5">
+                    <div>
+                         <h1 className="text-4xl font-medium">Contact Us</h1>
+                         <p className="my-4 font-light">Email, call or complete the form to reach us and give review</p>
+                         <h3 className="mb-4 font-light">sixkilolounge@gmail.com</h3>
+                         <h3 className="mb-4 font-light">+251-987-654-321</h3>
+                    </div>
+                    <div className="md:flex md:justify-items-start md:gap-5">
+                         <ContactCatagories
+                              title="Feedback and Suggestions"
+                              passage = "We value your feedback and are countinuously working to improve our Lounge. Your review and feedback are crucial in shaping the future of our Lounge"
+                         />
+                         <ContactCatagories
+                              title="Media inquiries"
+                              passage="For media-related questions, please contact us at media@sixkilolounge.com" 
+                         />
+                    </div>
+               </div>
+               <Form/>
+          </div>
+     )
+}
+
+export default ContactMessage
