70aba1c63fb5e8f5142b5bce720725a6fefe5b45
Medan <medanabebe17@gmail.com>
2025-04-10 20:20:57 +0300

    added profile page

---
+++ Frontend/src/pages/profile.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/shared/header"
+import Footer from "../components/shared/footer"
+import DeliveryForm from "../components/profile/delivery_form"
+
+const profile = () => {
+     return (
+          <div>
+               <Header />
+               <DeliveryForm/>
+               <Footer />
+          </div>
+     )
+}
+
+export default profile
