04180a1c29ba19ed0654fc7bee59af6c5ea8d3ea
Medan <medanabebe17@gmail.com>
2025-04-10 12:45:21 +0300

    Added Contact page

---
+++ Frontend/src/pages/contact.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/shared/header"
+import Footer from "../components/shared/footer"
+import ContactMessage from "../components/contact/Contact_message"
+const contact = () => {
+     return (
+          <div>
+               <Header />
+               <ContactMessage/>
+               <Footer />
+          </div>
+     )
+}
+
+export default contact
