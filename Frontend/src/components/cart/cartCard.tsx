94966c32f4fcd178824902c45e5fc5d5a6636775
Medan <medanabebe17@gmail.com>
2025-04-10 13:35:18 +0300

    styled conditionally the cartCard

--- Frontend/src/components/cart/cartCard.tsx
+++ Frontend/src/components/cart/cartCard.tsx
@@ -16,13 +16,13 @@ const cartCard = () => {
      const cartItems = cart?.cart.cart || []
 
      return (
-          <div className="bg-white shadow-2xl rounded-[30px] py-5 px-6 mx-5 min-w-[260px] md:px-7 md:min-w-[312px] lg:mx-16 md:h-[450px] overflow-hidden">
-               <h1 className="text-2xl font-medium">My Orders</h1>
-               <div className="md:grid md:grid-cols-2 md:place-items-center">                    
+          <div className={cartItems.length === 0 ? "bg-white shadow-2xl rounded-[30px] py-10 px-6 mx-5 min-w-[260px] md:px-7 md:h-[300px] md:min-w-[312px] lg:mx-16 " :  "bg-white shadow-2xl rounded-[30px] py-5 px-6 mx-5 min-w-[260px] md:px-7 md:min-w-[312px] lg:mx-16 md:h-[450px] overflow-hidden"}>
+               <h1 className="text-4xl font-medium">My Orders</h1>
+               <div className={cartItems.length === 0 ? "": "md:grid md:grid-cols-2 md:place-items-center"}>                    
                     <div className="scroll overflow-y-scroll max-h-[450px]">
                          {
                               cartItems.length === 0 ? (
-                                   <div className="my-18 text-3xl font-medium md:text-4xl">No food items in your cart <FontAwesomeIcon icon={faHeartCrack} className="text-red-600 text-3xl" /></div>
+                                   <div className="my-18 text-3xl font-light md:text-3xl">There are no food items in your cart <FontAwesomeIcon icon={faHeartCrack} className="text-red-600 text-3xl" /></div>
                               ) : (
                                    
                                    cartItems.map((food: CartItemType) => (
