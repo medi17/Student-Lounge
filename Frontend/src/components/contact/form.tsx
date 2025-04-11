349c38504d80ee7fe7e10cd4c4464049a3062fe8
Medan <medanabebe17@gmail.com>
2025-04-11 18:19:19 +0300

    fixed the opacity of the place holders on inputs

--- Frontend/src/components/contact/form.tsx
+++ Frontend/src/components/contact/form.tsx
@@ -6,17 +6,17 @@ const form = () => {
                <p className="font-light text-sm my-2">You can reach us anytime</p>
                <form>
                     <div className="md:flex md:justify-center md:items-center md:gap-3">
-                         <input className="bg-gray-hepta py-2 px-4 w-full my-3 rounded-3xl focus:outline-none"
+                         <input className="bg-gray-hepta text-gray-tri py-2 px-4 w-full my-3 rounded-3xl focus:outline-none"
                               type="text"
                               placeholder="First name" />
-                         <input className="bg-gray-hepta py-2 px-4 w-full my-3 rounded-3xl focus:outline-none"
+                         <input className="bg-gray-hepta text-gray-tri py-2 px-4 w-full my-3 rounded-3xl focus:outline-none"
                               type="text"
                               placeholder="Last name" />
                     </div>
-                    <input className="bg-gray-hepta py-2 px-4 w-full my-3 rounded-3xl focus:outline-none"
+                    <input className="bg-gray-hepta text-gray-tri py-2 px-4 w-full my-3 rounded-3xl focus:outline-none"
                          type="text"
                          placeholder="Your email" />
-                    <textarea className="bg-gray-hepta py-2 px-4 w-full h-[120px] my-3 rounded-3xl focus:outline-none"
+                    <textarea className="bg-gray-hepta text-gray-tri py-2 px-4 w-full h-[120px] my-3 rounded-3xl focus:outline-none"
                          name="message"
                          placeholder="Your message" />
                     <button className="bg-Crimson text-white font-medium w-full p-2 rounded-3xl cursor-pointer border-2 border-Crimson hover:text-Crimson hover:bg-white"
