ec24710c18697f37febede5560e3ddc3cdc55e17
Medan <medanabebe17@gmail.com>
2025-04-16 23:26:38 +0300

    server.ts file on test

--- Backend/server.ts
+++ Backend/server.ts
@@ -2,4 +2,11 @@ import express from "express";
 
 const app = express();
 
-app.listen(3000)
+app.get("/", function (req, res){
+	res.send("Hello, World!");
+});
+
+app.listen(3000, () => {
+	console.log("now listnig on port 3000");
+	
+})
