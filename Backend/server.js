a8055b452c9e26e520cbac828c01a4801099bb00
Medan <medanabebe17@gmail.com>
2025-06-16 21:33:26 +0300

    updated server.js

---
+++ Backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors"
+import { connectDb } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+
+
+// app config
+const app = express();
+const port = 4000
+
+
+// middleware
+app.use(express.json())
+app.use(cors())
+
+
+// Database connection
+connectDb()
+
+
+// api endpoints
+app.use("/api/food", foodRouter)
+app.use("/images", express.static('uploads'))
+
+app.get("/", function (req, res){
+	res.send("Helllllllllo, World!");
+})
+
+app.listen(port, () => {
+	console.log(`now listning on port ${port}`)
+})
