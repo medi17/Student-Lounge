34b2e7271a80540cc74bc7e11b713ec33f460c74
Medan <medanabebe17@gmail.com>
2025-06-16 23:29:07 +0300

    added admin page

---
+++ Frontend/src/pages/admin.tsx
@@ -0,0 +1,14 @@
+import AdminMain from '../components/Admin/admin_main'
+import AdminHeader from '../components/Admin/admin_header'
+
+
+const admin = () => {
+     return (
+          <div>
+               <AdminHeader />
+               <AdminMain />
+          </div>
+     )
+}
+
+export default admin
