4f8debe4458feb76bb69a8c88db733edcd9f4114
Medan <medanabebe17@gmail.com>
2025-06-23 22:49:38 +0300

    updated admin page

--- Frontend/src/pages/admin.tsx
+++ Frontend/src/pages/admin.tsx
@@ -1,10 +1,11 @@
 import AdminMain from '../components/Admin/admin_main'
 import AdminHeader from '../components/Admin/admin_header'
-
+import { ToastContainer } from 'react-toastify';
 
 const admin = () => {
      return (
           <div>
+               <ToastContainer/>
                <AdminHeader />
                <AdminMain />
           </div>
