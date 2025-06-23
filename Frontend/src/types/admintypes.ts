18fae24e52a74e0fa959cdc64773949da75aebaf
Medan <medanabebe17@gmail.com>
2025-06-23 22:49:15 +0300

    added admin page types

---
+++ Frontend/src/types/admintypes.ts
@@ -0,0 +1,29 @@
+export type AddItemType = {
+     name: string,
+     duration: string,
+     description: string,
+     price: string,
+     catagory: string,
+}
+type addPayload = {
+     name: string,
+     value: string
+}
+export type AddStateType = {
+     add: AddStateType[]
+}
+
+export type AddReducerAction = {
+     type: string,
+     payload?: addPayload
+}
+
+export type itemListType = {
+     _id: string,
+     name: string,
+     duration: string,
+     description: string,
+     price: string,
+     catagory: string,
+     image: string,
+}
