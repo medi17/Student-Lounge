deebaea5c2f0103a467077ac2077db1c0fdf17a7
Medan <medanabebe17@gmail.com>
2025-06-28 18:47:04 +0300

    updated cartContext

--- Frontend/src/context/CartContext.tsx
+++ Frontend/src/context/CartContext.tsx
@@ -1,6 +1,8 @@
-import { createContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer, useState } from "react";
 import cartReducer, { InitialState } from "../hooks/UseReducer";
 import { CartContextType, ChildrenType } from "../types/foodTypes";
+import axios from "axios";
+import { UserContext } from "./UserContext";
 import { foodItems } from "../data";
 
 export const CartContext = createContext<CartContextType | null>(null)
@@ -9,8 +11,37 @@ export const CartContext = createContext<CartContextType | null>(null)
 const CartContextProvider = ({ children }:ChildrenType) => {
      const [cart, dispatch] = useReducer(cartReducer, InitialState)
 
+     // User context import
+     const usecontext = useContext(UserContext)
+
+     if (!usecontext) {
+          throw new Error("UserContext is not provided");
+     }
+     const setToken = usecontext.setToken
+     const url = usecontext.url
+
+     // a code for fetching food lists from db to display
+     const [foodItems, setFoodItems] = useState([])
+
+     const fetchFoods = async () => {
+          const response = await axios.get(url + "/api/food/list")
+          setFoodItems(response.data.data)
+     }
+
+     // when the browser loads it constantly fetchs list of foods
+     useEffect(() => {
+          async function loadFoods() {
+               await fetchFoods()
+               const storedToken = localStorage.getItem("token");
+               if (storedToken) {
+                    setToken(storedToken);
+               }
+          }
+          loadFoods();
+     }, [])
+     
      return (
-          <CartContext.Provider value={{ cart, dispatch, foodItems }}>
+          <CartContext.Provider value={{ cart, dispatch, foodItems}}>
                {children}
           </CartContext.Provider>
      )
