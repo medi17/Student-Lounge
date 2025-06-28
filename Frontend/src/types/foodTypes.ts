35efd91ab6838913ed8f7fc4a7fa25b980c032b2
Medan <medanabebe17@gmail.com>
2025-06-28 15:39:53 +0300

    changes the name of the file

---
+++ Frontend/src/types/foodTypes.ts
@@ -0,0 +1,64 @@
+// // General Context types
+
+export type ChildrenType = {
+  children: React.ReactNode
+}
+
+// // CartContext types
+
+export type FoodItem = {
+  id: number,
+  name: string,
+  duration: string,
+  description: string,
+  price: string,
+  image: string,
+  catagory: string,
+  quantity:number,
+}
+
+export type FoodProps = {
+  food: FoodItem,
+}
+
+export type CartContextType = {
+  cart: CartStateType,
+  dispatch: React.ActionDispatch<[action: ReducerAction]>
+  foodItems: FoodItem[]
+}
+
+export type CartItemType = {
+  id: number,
+  name: string,
+  duration: string,
+  description: string,
+  price: string,
+  image: string,
+  catagory: string,
+  quantity:number,
+}
+
+export type CartStateType = {
+  cart: CartItemType[]
+}
+
+const Reducer_action_type = {
+  ADD: "ADD",
+  REMOVE: "REMOVE",
+  INCREASE: "INCREASE",
+  DECREASE: "DECREASE",
+}
+
+export type ReducerActionType = typeof Reducer_action_type
+
+export type ReducerAction = {
+  type: string,
+  payload: CartItemType 
+}
+
+// // Order Context types
+
+export type OrderContextType ={
+  isChecked: string, 
+  setIsChecked: React.Dispatch<React.SetStateAction<string>>
+}
