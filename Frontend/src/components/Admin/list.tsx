c2381c78da8d03c0f15a25f57d026664a9e9d56b
Medan <medanabebe17@gmail.com>
2025-07-03 23:30:11 +0300

    added styles

--- Frontend/src/components/Admin/list.tsx
+++ Frontend/src/components/Admin/list.tsx
@@ -40,34 +40,40 @@ const list = ({ className }: Props) => {
 
      return (
           <div className={className}>
-               <table className="mt-5 w-full border-2 border-gray-tetra rounded-3xl bg-gray-hepta">
-                    <thead>
-                         <tr>
-                              <th className="font-medium">Image</th>
-                              <th className="font-medium">Name</th>
-                              <th className="font-medium">Catagory</th>
-                              <th className="font-medium">Price</th>
-                              <th className="font-medium">Action</th>
-                         </tr>
-                    </thead>
-                    <tbody>
-                         {list.map((item:itemListType, index:number) => {
-                              return (
-                                   <tr key={index}>
-                                        <td>
-                                             <img src={`${url}/images/` + item.image} alt="item image" />
-                                        </td>
-                                        <td>{item.name}</td>
-                                        <td>{item.catagory}</td>
-                                        <td>{item.price}</td>
-                                        <td className="cursor-pointer"
-                                             onClick={() => removeFoodItem(item._id)}
-                                        >Delete</td>
-                                   </tr>
-                              )
-                         })}
-                    </tbody>
-               </table>    
+               <div className="rounded-[30px] overflow-hidden">
+                    <table className="mt-5 w-full border-gray-tetra rounded-2xl shadow-2xl border-collapse">
+                         <thead className="round-t-4xl bg-gray-hexa text-xs sm:text-base">
+                              <tr>
+                                   <th className="font-medium py-6">Image</th>
+                                   <th className="font-medium py-6">Name</th>
+                                   <th className="font-medium py-6">Catagory</th>
+                                   <th className="font-medium py-6">Price</th>
+                                   <th className="font-medium py-6">Action</th>
+                              </tr>
+                         </thead>
+                         <tbody className="[&_tr:nth-child(even)]:bg-[var(--color-gray-tri)]">
+                              {list.map((item:itemListType, index:number) => {
+                                   return (
+                                        <tr key={index} className="">
+                                             <td className="py-1 flex justify-center">
+                                                  <div className="w-10 h-10 md:w-20 md:h-18">
+                                                       <img src={`${url}/images/` + item.image} alt="item image"
+                                                       className="w-full h-full" 
+                                                       />
+                                                  </div>
+                                             </td>
+                                             <td className="text-center">{item.name}</td>
+                                             <td className="text-center">{item.catagory}</td>
+                                             <td className="text-center">{item.price}</td>
+                                             <td className="cursor-pointer text-center text-Crimson text-lg font-bold"
+                                                  onClick={() => removeFoodItem(item._id)}
+                                             >x</td>
+                                        </tr>
+                                   )
+                              })}
+                         </tbody>
+                    </table>    
+               </div>
           </div>
      )
 }
