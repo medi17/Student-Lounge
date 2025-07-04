2ae76c7cd8ce0c085b9c26fce783b822d6822b3d
Medan <medanabebe17@gmail.com>
2025-07-05 00:18:58 +0300

    changed the  to req.body.userid req.userId toget the token.id directly from req not client because it is get request

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -9,11 +9,11 @@ const authMiddleware = async (req, res, next) => {
 
      try {
           const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
-          req.body.userId = tokenDecode.id;
+          req.userId = tokenDecode.id;
           next();
      } catch (error) {
-          console.log(error)
-          res.json({success:false, message:"Error"})
+          console.log("Authentication Error:", error)
+          res.json({success:false, message:"Invalid or expired token"})
      }
 }
 
