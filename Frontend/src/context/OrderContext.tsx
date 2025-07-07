a39d422e02e505abe203953e5558196f2a136066
Medan <medanabebe17@gmail.com>
2025-07-07 19:44:36 +0300

    added foodPriceCalculator function tp orderContext

--- Frontend/src/context/OrderContext.tsx
+++ Frontend/src/context/OrderContext.tsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
-import { ChildrenType, OrderContextType } from "../types/foodTypes"
+import { createContext, useContext, useState } from "react";
+import { CartItemType, ChildrenType, OrderContextType } from "../types/foodTypes"
+import { CartContext } from "./CartContext";
 
 
 export const OrderContext = createContext<OrderContextType | null>(null)
@@ -9,8 +10,25 @@ export const OrderContext = createContext<OrderContextType | null>(null)
 const OrderContextProvider = ({ children }: ChildrenType) =>{
      const [isChecked, setIsChecked] = useState("yes")
 
+     const cartContext = useContext(CartContext)
+
+     if (!cartContext) {
+          throw new Error("CartContext is not provided");
+     }     
+     const cartItems = cartContext.cart.cart
+     const foodPriceCalculator = () => {
+          let Total:number = 0
+          cartItems.map((foodItems:CartItemType) => {
+               return (
+                    Total += (Number(foodItems.price) * Number(foodItems.quantity))
+               )
+          })
+          return Total
+     }
+     const totalFee = foodPriceCalculator() + (isChecked === "yes" ? 10 : 0)
+
      return (
-          <OrderContext.Provider value= {{isChecked, setIsChecked}}>
+          <OrderContext.Provider value= {{isChecked, setIsChecked, foodPriceCalculator, totalFee}}>
                {children}
           </OrderContext.Provider>
      )
