cec06b409a344d4672caa54ab26002cc5c296446
Medan <medanabebe17@gmail.com>
2025-07-07 19:40:29 +0300

    moved foodPriceCalculator function into cartContext

--- Frontend/src/components/cart/orderSummary.tsx
+++ Frontend/src/components/cart/orderSummary.tsx
@@ -1,40 +1,20 @@
 import { useContext } from "react";
 import { OrderContext } from "../../context/OrderContext";
-import { CartContext } from "../../context/CartContext";
-import { CartItemType } from "../../types/foodTypes";
+import { useNavigate } from "react-router-dom";
 
 const OrderSummary = () => {
 
+     const navigator = useNavigate()
      const Order = useContext(OrderContext)
 
      if (!Order) {
           throw new Error("OrderContext is not provided");
      }
 
-     const isChecked = Order?.isChecked
+     const isChecked = Order.isChecked
+     const totalFee = Order.totalFee
+     const foodPriceCalculator = Order.foodPriceCalculator
 
-     const cartContext = useContext(CartContext)
-
-     if (!cartContext) {
-          throw new Error("CartContext is not provided");
-     }
-     const cart = cartContext?.cart
-     const cartItems = cart?.cart
-
-
-     const foodPriceCalculator = () => {
-          let Total:number = 0
-          cartItems?.map((foodItems: CartItemType) => {
-               return (
-                    Total += (Number(foodItems.price) * Number(foodItems.quantity))
-               )
-          })
-          return Total
-     }
-
-     if (!Order) {
-          throw new Error("OrderContext is not provided");
-     }
 
      return (
           <>
@@ -51,12 +31,13 @@ const OrderSummary = () => {
                     </div>
                     <div className="flex justify-between items-center gap-4 lg:gap-42 mt-3">
                          <h2 className="text-lg font-[600]">Total</h2>
-                         <p className="font-[600]">{foodPriceCalculator() + (isChecked === "yes" ? 10 : 0)} ETB</p>
+                         <p className="font-[600]">{totalFee} ETB</p>
                     </div>
                </div>
                <hr className="my-5 w-full border-dashed" />
                <button className="bg-Crimson text-white py-2 lg:px-24 rounded-3xl font-medium px-14 lg:text-lg cursor-pointer border-2 border-Crimson hover:text-Crimson hover:bg-white"
-               >Confirm Order</button>
+               onClick={() => navigator("/profile")}
+               >Proceed to Order</button>
 
           </>
 
