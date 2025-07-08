7786bd047dd4955ff5f3e0493b2ff873ea5d2265
Medan <medanabebe17@gmail.com>
2025-07-08 23:23:37 +0300

    added my orders page

---
+++ Frontend/src/pages/myorders.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/shared/header"
+import Footer from "../components/shared/footer"
+import Myorders from "../components/orders/myorders"
+
+const myOrders = () => {
+     return (
+          <div>
+               <Header />
+               <Myorders/>
+               <Footer />
+          </div>
+     )
+}
+
+export default myOrders
+
