6ddc8ee422b2a3385a77d1d8018b13da1d85802d
Medan <medanabebe17@gmail.com>
2025-07-08 23:19:03 +0300

    added two routes on order router

--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { placeOrders } from "../controllers/orderController.js";
+import { placeOrders, userOrders, fetchOrders } from "../controllers/orderController.js";
 import authMiddleware from "../middleware/auth.js";
 
 const orderRouter = express.Router();
 
-orderRouter.post("/placeorder",authMiddleware, placeOrders)
+orderRouter.post("/placeorder", authMiddleware, placeOrders)
+orderRouter.get("/myorders", authMiddleware, userOrders)
+orderRouter.get("/usersorders", fetchOrders)
 
 export default orderRouter
