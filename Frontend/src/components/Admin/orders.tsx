c19b6938d5c6dac5abe42db49072f8f67a670a15
Medan <medanabebe17@gmail.com>
2025-07-08 23:20:01 +0300

    added orders component

--- Frontend/src/components/Admin/orders.tsx
+++ Frontend/src/components/Admin/orders.tsx
@@ -1,12 +1,57 @@
+import { useContext, useEffect, useState } from "react"
+import { UserContext } from "../../context/UserContext"
+import axios from "axios"
+import { orderTypes } from "../../types/userTypes"
+import OrdersCard from "../Admin/ordersCard"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faFaceSadTear } from "@fortawesome/free-solid-svg-icons"
+import orderData from "../../data_admin"
 
 type Props = {
      className:string
 }
 
-const orders = ({className}: Props) => {
+const orders = ({ className }: Props) => {
+     
+     const [orders, setOrders] = useState<orderTypes[]>([])
+
+     // User context import
+     const usecontext = useContext(UserContext)
+
+     if (!usecontext) {
+          throw new Error("UserContext is not provided");
+     }
+     const url = usecontext.url
+
+
+     const fetchOrders = async () => {
+          const response = await axios.get(`${url}/api/order/usersorders`)
+          setOrders(response.data.data)
+     }
+
+     useEffect(() => {
+          // fetchOrders(); 
+          setOrders(orderData)
+     }, [])
+
      return (
-          <div className={className}>
-               <h2>Orders</h2>
+          <div className={className}> 
+               <div className={orders.length === 0 ? "w-full bg-white shadow-2xl rounded-[30px] py-10 px-6 my-5" :  "w-full bg-white shadow-2xl rounded-[30px] py-5 px-6 my-5"}>
+                    <div className="mt-7 flex flex-col gap-5">
+                         {orders.length === 0 ? (
+                              <div>
+                                   <div className="my-18 text-3xl flex items-center font-light md:text-3xl">Seems like there are no orders
+                                        <FontAwesomeIcon icon={faFaceSadTear} className="text-red-600 text-4xl pl-2" />
+                                   </div>
+                              </div>
+                    ) : (
+                              orders.map((order:orderTypes) => (
+                                        <OrdersCard order={order} key={order._id}></OrdersCard>
+                                   ))                                   
+                              )
+                         }
+                    </div>                    
+               </div>
           </div>
      )
 }
