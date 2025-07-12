8a2b1791ce828bde6b968cf58846e5bce56db4cc
Medan <medanabebe17@gmail.com>
2025-07-12 21:08:25 +0300

    added loading page animation

--- Frontend/src/components/home/catagories/mainfoods.tsx
+++ Frontend/src/components/home/catagories/mainfoods.tsx
@@ -4,25 +4,29 @@ import { faFaceSadTear } from "@fortawesome/free-solid-svg-icons"
 import { useContext } from "react"
 import { FoodItem } from "../../../types/foodTypes"
 import { CartContext } from "../../../context/CartContext"
+import SkeletonFood from "./skeletonFoodCard"
 
 const Mainfoods = () => {
 
      const FoodListObject = useContext(CartContext)
-     const mainItems = FoodListObject?.foodItems || []
+     const mainItems = FoodListObject?.foodItems
+     const foodLoading = FoodListObject?.foodLoading
 
      if (!mainItems) {
           return <div className="ml-5 text-2xl font-bold">Sorry no food items are available <FontAwesomeIcon icon={faFaceSadTear} className="text-yellow-600 text-3xl"/></div>;
      }
-     return (
+     return foodLoading ? (
+     <SkeletonFood/>
+                     
+     ) : (
+               
           <div className="scroll main bg-white flex justify-start gap-5 overflow-x-scroll mb-5 mx-2 rounded-[30px] md:gap-10 px-3 py-5 md:px-10">
-               {
-                    mainItems.map((fooditem: FoodItem) => (                      
-                         fooditem.catagory === "Main foods" && (     
-                              <FoodComponent food = { fooditem } key={fooditem._id}/>
-                         )
-                    
-                    ))
-               }
+
+               {mainItems.map((fooditem: FoodItem) => (
+                    fooditem.catagory === "Main foods" && (
+                         <FoodComponent food={fooditem} key={fooditem._id} />
+                    )
+               ))}
           </div>
      )
 }
