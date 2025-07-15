0af404dd2c615ba8db48e2c4211d5fb7401cc4fd
Medan <medanabebe17@gmail.com>
2025-07-15 21:35:42 +0300

    added mobile

---
+++ Frontend/src/pages/mobile.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/shared/header"
+import MobileCard from "../components/mobile/mobileCard"
+import Footer from "../components/shared/footer"
+const Mobile = () => {
+     return (
+          <div>
+               <Header />
+               <MobileCard />
+               <Footer />
+          </div>
+     )
+}
+
+export default Mobile
