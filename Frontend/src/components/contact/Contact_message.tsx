735f82f6d288d6bb0b9945046642007a0f0129cd
Medan <medanabebe17@gmail.com>
2025-07-16 02:40:42 +0300

    added ease up and from left animations

--- Frontend/src/components/contact/Contact_message.tsx
+++ Frontend/src/components/contact/Contact_message.tsx
@@ -7,10 +7,10 @@ const ContactMessage = () => {
           <div className="m-5 py-3 px-7 rounded-3xl lg:mx-16 md:flex md:justify-between md:gap-10 lg:gap-20">
                <div className="pt-5">
                     <div>
-                         <h1 className="text-4xl font-medium">Contact Us</h1>
-                         <p className="my-4 font-light">Email, call or complete the form to reach us and give review</p>
-                         <h3 className="mb-4 font-light">sixkilolounge@gmail.com</h3>
-                         <h3 className="mb-4 font-light">+251-987-654-321</h3>
+                         <h1 id="p-1" className="from-left text-4xl font-medium">Contact Us</h1>
+                         <p id="p-2" className="from-left my-4 font-light">Email, call or complete the form to reach us and give review</p>
+                         <h3 id="p-3" className="from-left mb-4 font-light">sixkilolounge@gmail.com</h3>
+                         <h3 id="p-4" className="from-left mb-4 font-light">+251-987-654-321</h3>
                     </div>
                     <div className="md:flex md:justify-items-start md:gap-5">
                          <ContactCatagories
