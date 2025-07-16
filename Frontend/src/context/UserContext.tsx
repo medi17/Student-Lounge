20c0d97b75022d299c0a170c8c43364982615629
Medan <medanabebe17@gmail.com>
2025-07-16 20:49:31 +0300

    fixed the backend url

--- Frontend/src/context/UserContext.tsx
+++ Frontend/src/context/UserContext.tsx
@@ -6,7 +6,7 @@ export const UserContext = createContext<UserContextType | null>(null)
 
 export const UserContextProvider = ({ children }:ChildrenType) => {
 
-     const url = "https://student-lounge-backend-jm9o.onrender.com/"
+     const url = "https://student-lounge-backend-jm9o.onrender.com"
      const [token, setToken] = useState("")
      const [toggleState, setToggleState] = useState(1)
      const [updating, setUpdating] = useState<boolean>(false)
